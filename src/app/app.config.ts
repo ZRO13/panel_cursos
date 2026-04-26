eb56cc63bd776f7e85b6edc9723b140c6cb359b3
Zero13Dev <juliocoloma1875@gmail.com>
2026-04-26 09:34:41 -0500

    fin2.1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,5 @@ import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes withHashLocation()) ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes ,withHashLocation()) ]
 };
